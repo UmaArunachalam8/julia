9ff48ada138e874b4b80edd1c12bc0550f9d9bb0
Uma <umaarunachalam888@gmail.com>
2017-06-15 00:14:59 +0530

    Add UCBEXP.jl.

---
+++ UCBEXP.jl
@@ -0,0 +1,37 @@
+narms=10;
+runs=1000;
+runmax=2000;
+Q=Array{Float64}(narms);
+N=Array{Int64}(narms);
+qstar=Array{Float64}(narms);
+R=Array{Float64}(runmax,runs)
+S=Array{Float64}(runs)
+W=Array{Float64}(narms);
+for i=1:runmax
+  for x=1:narms
+    Q[x]=0;
+    N[x]=0;
+    qstar[x]=randn();
+  end
+for j=1:runs
+  a=rand();
+  if(a<0.1)
+    c=2;
+    k=1-exp(-1*j);
+    W=Q+(c*(sqrt.(k./N)));
+    (m,p)=findmax(W);
+  else
+    (m,p)= findmax(Q);
+  end
+  R[i,j] = qstar[p]+randn();
+  N[p]+=1;
+  Q[p]=Q[p]+((1.0/N[p])*(R[i,j]-Q[p]));
+end
+end
+for b=1:runs
+    S[b]=0.0;
+    for y=1:runmax
+        S[b]=S[b]+R[y,b];
+    end
+end
+avr=S/runmax;
